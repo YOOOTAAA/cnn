0386ccdec8a088ccda78436c393ffaf251d91158
YOOOTAAA <yota888themovie@icloud.com>
2020-05-21 12:02:28 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "posts#index"
    get "/new" => "posts#new"
    post "/posts/create" => "posts#create"
    get "/index" => "posts#index"
    
-   root "posts#index"
+   
   
 end
